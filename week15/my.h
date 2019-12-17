f177317c10ecd3fed3843bde7da02eb4978d46c5
Zikigai <rbof1152786807@163.com>
2019-12-17 02:08:59 -0800

    daily

---
+++ week15/my.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<limits.h>
+#include<errno.h>
+#include<sys/types.h>
+#include<pthread.h>
+#include<semaphore.h>
+#define NUM 14
+#define TN 100
+#define WRITER FIRST
